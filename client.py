6e4e2f4b24b7e0f089afc0ce6ab4ae9c90a07cac
talhah <talhah.peerbhai@gmail.com>
2021-11-19 00:11:13 +0300

    Join and part channels, mark as read and un-read, create a tab/delete it but with a catch, it's just hidden and still resides in memory

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 # Author: Talhah Peerbhai
 # Email: hello@talhah.tech
+
+'''
+This file contains the Internet Relay Chat client, it imports the irc library which
+I made to abstract the IRC protocol wheras this program deals with the GUI and putting
+it all together. I'm going to try to break it into more files but at the moment
+this is it.
+'''
 from irclib import IrcCon
 # PySimpleGUI is a wrapper for tkinter and some other frameworks
 import PySimpleGUI as sg
@@ -13,6 +20,11 @@ sg.theme("Topanga")
 class EmptyValue(Exception):
     def __init__(self):
         self.message = "User Input is required"
+        super().__init__(self.message)
+
+class InvalidCommand(Exception):
+    def __init__(self):
+        self.message = "User gave an invalid command"
         super().__init__(self.message)
 
 class Client(IrcCon):
@@ -38,12 +50,30 @@ class Client(IrcCon):
             logging.warning(f"{errorType}")
             #(server,host,port,nick,user,rname) = loginWin(self.HOST,self.PORT,nick,user,rname)
             #self.login(nick,user,rname)
+    
+    def on_message(self,who,channel,msg):
+        msg = f"{who} > {msg}"
+        if channel == self.NICK:
+            channel = who
+        if channel not in self.channels:
+            create_tab(self.window,channel)
+            openTabs.append(channel)
+            self.channels.add(channel)
+        self.window[f"{channel}B"].update(msg + "\n",append=True)
+        markUnread(channel)
+
 
     def unknown_message(self,line):
-        msg = self.window["infoB"].get() + "\n" + line
-        self.window["infoB"].update(msg)
-        #print(line)
-        
+        # This used to "print" to infoB box which had all console output rerouted to it
+        # that would apparently lead to a race condition when printing too fast? 
+        # and lead to a seg fault error, hooray I guess!
+        # It is important to note that pysimplegui was overriding the builtin print
+        # so I'm talking about that print function. It wasn't transparent that it was
+        # overriding the default one and the bug may lie therein or it's some sort of 
+        # limitation. TODO Make a PR or github issue one day to fix this
+        line = line + "\n"
+        self.window["infoB"].update(line,append=True)
+
 # Window for displaying error messages
 def errorWin(message):
     errorLayout = [[sg.Text(f"{message}")],
@@ -138,23 +168,111 @@ def mainLayout():
         ]]
     return layout
 
+def create_tab(win,channel):
+    if channel in tabHist:
+        win[f"{channel}"].update(visible=True)
+    else:
+        element = [[sg.Multiline(size=(75, 15), font=('Helvetica 10'),key=f"{channel}B",autoscroll=True,disabled=True)]]
+        tab = sg.Tab(f"{channel}",element,key=channel)
+        win["chats"].add_tab(tab)
+        tabHist.append(channel)
+
+def delete_tab(win,channel):
+    win[f"{channel}"].update(visible=False)
+
+def markUnread(tab):
+    tabgroup = mainWin["chats"].Widget
+    for i in range(len(openTabs)):
+        if openTabs[i] == tab:
+            break
+    tab = mainWin[f"{tab}"]
+    title = tab.Title
+    if not title.startswith("*"):
+        title = "*" + title
+    tabgroup.tab(i,text=title)
+
+def markRead(tab):
+    temp = tab.rstrip("*")
+    tabgroup = mainWin["chats"].Widget
+    for i in range(len(openTabs)):
+        if openTabs[i] == temp:
+            break
+    title = openTabs[i]
+    tabgroup.tab(i,text=title)
+
+def processCommand(win,irc,query):
+    query = query.lstrip("/")
+    try:
+        if query == "":
+            raise InvalidCommand
+        query = query.split()
+        currentTab = vals1["chats"]
+        if query[0].lower() == "join":
+            channels = query[1:]
+            for chan in channels:
+                if chan not in irc.channels:
+                    openTabs.append(chan)
+                    irc.join(chan)
+                    create_tab(win,chan)
+        elif query[0].lower() == "part":
+            channels = query[1:]
+            for chan in channels:
+                if chan in irc.channels:
+                    openTabs.remove(chan)
+                    delete_tab(win,chan)
+                    irc.part(chan)
+                else:
+                    win[f"{currentTab}B"].update("Need to be in channel",append=True)                
+    except InvalidCommand:
+        logging.warning("User gave an invalid command")
+        win["infoB"].update("Unknown command\n",append=True)
+    finally:
+        win["msgbox"].update("")
+
+def sendMsg(win,irc,chan,msg):
+    if vals1["chats"] != "info":
+        win[f"{chan}B"].update(f"{irc.NICK} > " + msg + "\n",append=True)
+        irc.privmsg(f"{chan}",msg)
+    win["msgbox"].update("")
+
+# Initial login window
 (server,port,nick,user,rname) = loginWin("127.0.0.1","6667")
+# Initialize main window thereafter
 mainWin = sg.Window("Slick IRC",mainLayout(),font=("Helvetica","13"),default_button_element_size=(8,2),finalize=True)
+# Initialize irc client and connect
 irc = Client(mainWin)
 irc.connect(server,port)
 loggedIn = False
 failedLogin = False
 
+# All the tabs seen so far
 tabHist = ["info"]
+openTabs = ["info"]
 while True:
+    # Event and values
     ev1, vals1 = mainWin.read(timeout=10)
+    # We haven't sucessfully logged in yet
     if not loggedIn:
         irc.login(nick,user,rname)
         loggedIn = True
+    # We failed login, darn it, try again and display error
     if irc.failedLogin:
         errorWin("Nickname in use, try a different one!")
         (server,port,nick,user,rname) = loginWin(server,port,nick,user,rname)
         loggedIn = False
+    if ev1 == "SEND":
+        query = vals1["msgbox"].rstrip()
+        # Ignore bogus empty messages
+        if query != "":
+            # An IRC command
+            if query.startswith("/"):
+                processCommand(mainWin,irc,query)
+            else:
+                sendMsg(mainWin,irc,vals1["chats"],query)
+    if vals1["chats"] != "info":
+        if vals1["chats"].startswith("*"):
+            markRead(vals1["chats"])
+    # User wants to exit :(
     if ev1 == sg.WIN_CLOSED or ev1 == "EXIT":
         break
     
